72cf02e76286efd9216d8bf4e4dd8ce64572da3e
Thibaud Lamothe <thibaudlamothe@MacBook-Pro-de-Thibaud.local>
2020-11-22 18:51:59 +0100

    update gitignore and add tor file

---
+++ tor/tor/tor/spiders/iptester.py
@@ -0,0 +1,18 @@
+import logging
+import scrapy
+from bs4 import BeautifulSoup
+
+class IPTesterSpider(scrapy.Spider):
+    name = 'IPtester'
+    allowed_domains = ['icanhazip.com']
+    start_urls = (
+        'https://icanhazip.com',
+    )
+
+    def parse(self, response):
+        soup = BeautifulSoup(response.body, 'html.parser')
+        ip_address = soup.get_text().rstrip('\n')
+        if ip_address:
+            logging.info('IP ADDRESS = %s', ip_address)
+        else:
+            logging.info('IP ADDRESS NOT FOUND')
